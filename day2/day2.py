f917611aca12f211e84da666ef767afb29ffff92
Tyler Ryan <46329645+tylerryan00@users.noreply.github.com>
2023-12-07 20:24:58 +0000

    initial push for day 2

---
+++ day2/day2.py
@@ -0,0 +1,17 @@
+def pt1(games):
+
+
+def main():
+    with open("day2_input.txt", "r") as file:
+        list_of_games = [line.strip() for line in file.readlines()]
+
+    games = {}
+
+    for i, game in enumerate(list_of_games):
+        game 
+
+    print(pt1(games))
+
+
+if __name__ == '__main__':
+    main()
